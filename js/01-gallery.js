72f8798beb23326408b44c3cd37a4d4d0a983253
Illia Bidiuk <Core778@gmail.com>
2023-03-05 18:40:01 +0200

    first commit

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,38 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryContainer = document.querySelector('.gallery');
+function createGalleryCardsMarkup(items) {
+    return items.map(({ preview, original, description }) => {
+        return `
+    <div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+        <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}" />
+        </a>
+    </div>
+    `;
+    }).join('');
+};
+const cardsMarkup = createGalleryCardsMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', cardsMarkup);
 
-console.log(galleryItems);
+galleryContainer.addEventListener('click',(event) => {
+    event.preventDefault();
+    if (event.target.nodeName !== 'IMG') {
+        return;
+    }
+    
+  const BanerUrl = event.target.dataset.source;
+   
+  const instance = basicLightbox.create(`
+    <img src=${BanerUrl} width="800" height="600">
+`)
+
+instance.show()
+
+// instance.show()
+// instance.show(() => console.log('lightbox now visible'))
+});
