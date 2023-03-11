7e8d0f917ca9cf11a634655828b98f4b62378a64
Illia Bidiuk <Core778@gmail.com>
2023-03-11 22:08:58 +0200

    first commit

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,6 +2,7 @@ import { galleryItems } from './gallery-items.js';
 
 // Change code below this line
 const galleryContainer = document.querySelector('.gallery');
+
 function createGalleryCardsMarkup(items) {
     return items.map(({ preview, original, description }) => {
         return `
@@ -9,36 +10,11 @@ function createGalleryCardsMarkup(items) {
   <img class="gallery__image" src="${preview}" alt="${description}" />
 </a></li>`;
     }).join('');
+     
 };
 const cardsMarkup = createGalleryCardsMarkup(galleryItems);
 galleryContainer.insertAdjacentHTML('beforeend', cardsMarkup);
-
-
-galleryContainer.addEventListener('click',(event) => {
-    event.preventDefault();
-    if (event.target.nodeName !== 'IMG') {
-        return;
-    }
-    var lightbox = new SimpleLightbox('.gallery a',  {
-        captionSelector: 'img',
-        captionsData: 'alt',
-        captionPosition: 'bottom',
-        captionDelay: 250,
-        scrollZoom: false,
-       });
-    
-//   const BanerUrl = event.target.dataset.source;
-   
-//   const instance = basicLightbox.create(`
-//     <img src=${BanerUrl} width="800" height="600">
-// `)
-
-// instance.show()
-// function onCloseModal(event) {
-//     if (event.code === 'Escape') {
-//     instance.close()
-// }}
-
-// window.addEventListener('keydown',onCloseModal);
-
-});
+new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+  });
